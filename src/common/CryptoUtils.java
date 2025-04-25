a498d52998329d86f60808786a070e44b5c49fb2
EB-Merrimack <144274749+EB-Merrimack@users.noreply.github.com>
2025-04-25 16:46:16 -0400

    intializing drm

---
+++ src/common/CryptoUtils.java
@@ -0,0 +1,21 @@
+package common;
+
+import javax.crypto.Cipher;
+
+import javax.crypto.SecretKey;
+
+
+public class CryptoUtils {
+
+    public static byte[] encrypt(String data, SecretKey key) throws Exception {
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.ENCRYPT_MODE, key);
+        return cipher.doFinal(data.getBytes());
+    }
+
+    public static String decrypt(byte[] encryptedData, SecretKey key) throws Exception {
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.DECRYPT_MODE, key);
+        return new String(cipher.doFinal(encryptedData));
+    }
+}
