637b604de3baae3f3fac8a55e9ee29503f73af08
Emily Brown <144274749+EB-Merrimack@users.noreply.github.com>
2025-04-30 10:57:40 -0400

    8 secure real time transport protocol intalization (#9)
    * starting video security
    
    * making video database a folder instead of a file
    
    * changing location of test cartoons so admins can add them and have them as encrypted files
    
    * renaming and organizing
    
    * adding in admin file
    
    * adding citations
    
    * adding back in drm
    
    * adding back in drm

---
+++ src/common/Stream_Security/SRTS.java
@@ -0,0 +1,58 @@
+package common.Stream_Security;
+
+import javax.crypto.SecretKey;
+
+import common.Stream_Security.encryption.EncryptPayload;
+
+import java.io.ByteArrayOutputStream;
+import java.net.*;
+import java.nio.ByteBuffer;
+
+import static java.util.Arrays.copyOfRange;
+/*
+ * This code is adapted from:
+ *   https://github.com/alesordo/Secure-RTSP-Video-Streaming
+ * 
+ * Original project: Secure RTSP Video Streaming (by Alessandro Sordo)
+ * Licensed under the MIT License.
+ * 
+ * Modifications:
+ */
+public class SRTS extends DatagramSocket {
+    public SRTS() throws SocketException {
+        super();
+    }
+    public SRTS(SocketAddress bindaddr) throws SocketException {
+        super(bindaddr);
+    }
+    public void mySend(DatagramPacket p, byte[] buff, int ptSize, SecretKey key, SecretKey hMacKey, String algorithm) throws Exception {
+        byte[] fixedheader = {0b00010000};
+
+        //Encryption here
+        EncryptPayload encryptPayload = new EncryptPayload();
+        byte[] encryptedPayload = encryptPayload.encrypt(buff,ptSize,key,hMacKey,algorithm);
+
+        //Merging everything
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream( );
+        outputStream.write(fixedheader);
+        outputStream.write(encryptedPayload);
+        byte[] frame = outputStream.toByteArray();
+
+        p.setData(frame, 0, frame.length);
+        super.send(p);
+    }
+    public byte[] myReceive(DatagramPacket inPacket, SecretKey key, SecretKey hMacKey, String algorithm) throws Exception {
+        super.receive(inPacket);
+
+        //Get the full packet
+        byte[] packet = inPacket.getData();
+        byte[] ctSizeHeader = copyOfRange(packet,1,3);
+        int ctSize = ByteBuffer.wrap(ctSizeHeader).getShort();
+
+        byte[] encryptedBuff = copyOfRange(packet,3,packet.length);
+
+        //Decription here
+        EncryptPayload encryptPayload = new EncryptPayload();
+        return encryptPayload.decrypt(ctSize,encryptedBuff,key,hMacKey,algorithm);
+    }
+}
---
+++ src/common/Stream_Security/encryption/KeyManager.java
@@ -0,0 +1,87 @@
+package common.Stream_Security.encryption;
+
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.*;
+import java.security.KeyStore;
+import java.util.Properties;
+/*
+ * This code is adapted from:
+ *   https://github.com/alesordo/Secure-RTSP-Video-Streaming
+ * 
+ * Original project: Secure RTSP Video Streaming (by Alessandro Sordo)
+ * Licensed under the MIT License.
+ * 
+ * Modifications:
+ */
+public class KeyManager {
+    private static KeyStore createKeyStore(String fileName, String pw, String algorithm) throws Exception {
+        File file = new File(fileName);
+        final KeyStore keyStore = KeyStore.getInstance("PKCS12");
+        if (file.exists()) {
+            // .keystore file already exists => load it
+            keyStore.load(new FileInputStream(file), pw.toCharArray());
+        } else {
+            // .keystore file not created yet => create it and save the keys
+            keyStore.load(null, null);
+
+            //Generate a secret key for encryption - the same for all the blocks
+            String[] fields= algorithm.split("/");
+            SecretKey secretKey = KeyGenerator.getInstance(fields[0]).generateKey();
+
+            //Generate a secret key for mac
+            SecretKey hMacKey = new SecretKeySpec(secretKey.getEncoded(), "HmacSHA512");
+
+            //Store the secret key
+            KeyStore.SecretKeyEntry keyStoreEntry = new KeyStore.SecretKeyEntry(secretKey);
+            KeyStore.PasswordProtection keyPassword = new KeyStore.PasswordProtection("pw-secret".toCharArray());
+            keyStore.setEntry("mySecretKey", keyStoreEntry, keyPassword);
+            keyStore.store(new FileOutputStream(fileName), pw.toCharArray());
+
+            //Store the hmac key
+            KeyStore.SecretKeyEntry keyStoreHMacEntry = new KeyStore.SecretKeyEntry(hMacKey);
+            keyStore.setEntry("hMacSecretKey",keyStoreHMacEntry,keyPassword);
+            keyStore.store(new FileOutputStream(fileName),pw.toCharArray());
+        }
+        return keyStore;
+    }
+    public static SecretKey[] getKeys() throws Exception{
+        SecretKey[] secretKeys = new SecretKey[2];
+        String[] parameters = getParameters();
+
+        String fileName = parameters[1];
+        String pw = parameters[2];
+        String algorithm = parameters[0];
+
+        //Retrieve/create KeyStore file
+        KeyStore keyStore = createKeyStore(fileName,pw,algorithm);
+
+        //Retrieve keys
+        KeyStore.PasswordProtection keyPassword = new KeyStore.PasswordProtection("pw-secret".toCharArray());
+
+        KeyStore.Entry keyEntry = keyStore.getEntry("mySecretKey", keyPassword);
+        secretKeys[0] = ((KeyStore.SecretKeyEntry) keyEntry).getSecretKey();
+
+        KeyStore.Entry hMacKeyEntry = keyStore.getEntry("hMacSecretKey", keyPassword);
+        secretKeys[1] = ((KeyStore.SecretKeyEntry) hMacKeyEntry).getSecretKey();
+        return secretKeys;
+    }
+    public static String[] getParameters() throws Exception{
+        try{
+            InputStream inputStream = new FileInputStream("security/configSecurity.properties");
+            Properties properties = new Properties();
+            properties.load(inputStream);
+
+            String algorithm = properties.getProperty("algorithm");
+            String keyStorePath = properties.getProperty("keyStorePath");
+            String keyStorePass = properties.getProperty("keyStorePass");
+
+            return new String[]{algorithm,keyStorePath,keyStorePass};
+        }
+        catch(Exception e){
+            System.err.println("Security configuration file not found!");
+            throw e;
+        }
+    }
+}
